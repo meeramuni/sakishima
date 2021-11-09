4e4498cd6d27b4532427cd4ccf5e91d3b86b1da9
vivianrdu <vivian_du@berkeley.edu>
2021-11-09 13:38:09 -0800

    fixes

--- CH 1 Memorization/code.js
+++ CH 1 Memorization/code.js
@@ -1,10 +1,20 @@
 
 function readTextFile(file)
 {
-  var fs = require("fs");
-  var text = fs.readFileSync(file).toString('utf-8');
-  var textByLine = text.split("\n");
-  return textByLine
+  var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send(null);
 }
 
 var Names = readTextFile("CH 1 Memorization/ch1_vocab.txt");
