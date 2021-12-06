740e8a0cf83dcc59cf86a36194c7d19093d7a606
vivianrdu <vivian_du@berkeley.edu>
2021-12-06 12:38:45 -0800

    update js

--- memorization/code.js
+++ memorization/code.js
@@ -78,7 +78,7 @@ $(document).ready(function () {
 
   function vocabOfLesson() {
     var tempVocab = [];
-    for (const vocab of Names) {
+    for (let vocab of Names) {
       if (Names[vocab] == currentLesson) {
         tempVocab.push(vocab);
       }
