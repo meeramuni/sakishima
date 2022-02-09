9f15b8d55241809705949e5ba8465a510588a683
Raksit Lau-Preechathammarach <raksit@berkeley.edu>
2022-02-09 13:40:25 -0800

    Fixed base link for audio sounds (was originally ocf, now github)

--- webapp/js/audio.js
+++ webapp/js/audio.js
@@ -5,6 +5,6 @@ support = !! (document.createElement('audio').canPlayType);
 if (support) clips = [];
 
 function soundfile(sound) {
- if (!clips[sound]) clips[sound] = new Audio('https://www.ocf.berkeley.edu/~sakishima/webapp/audio/'+sound);
+ if (!clips[sound]) clips[sound] = new Audio('meeramuni.github.io/sakishima/webapp/audio/'+sound);
  clips[sound].play();
 }
